8efd8c22acbfbda57386297445b70e791bb0101d
pakapaka <pakapaka@DESKTOP-01A825I>
2025-03-03 15:45:15 +0400

    Добавьте файлы проекта.

---
+++ PetMarketRfullApi/Domain/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetMarketRfullApi.Domain.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PetMarketRfullApi/Domain/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace PetMarketRfullApi.Domain.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int Quanity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Status { get; set; }
+        public int PetId { get; set; }
+        public Pet Pet { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
