c3a3c6b8e124f41a8d481a3554459beadf966110
pakapaka <pakapaka@DESKTOP-01A825I>
2025-03-04 19:16:21 +0400

    Add BaseRep, CatRep(GetAll only)

---
+++ PetMarketRfullApi/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetMarketRfullApi.Data.Contexts;
+using PetMarketRfullApi.Domain.Models;
+using PetMarketRfullApi.Domain.Repositories;
+
+namespace PetMarketRfullApi.Data.Repositories
+{
+    public class CategoryRepository : BaseRepositories, ICategoryRepository
+    {
+        // Конструктор для внедрения зависимости через DI
+        public CategoryRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+
+        public Task AddCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _appDbContext.Category.ToListAsync();
+        }
+
+        public Task<Category> GetCategoryByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PetMarketRfullApi/Program.cs
+++ PetMarketRfullApi/Program.cs
@@ -2,6 +2,8 @@ using PetMarketRfullApi.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using PetMarketRfullApi.Domain.Services;
 using PetMarketRfullApi.Sevices;
+using PetMarketRfullApi.Domain.Repositories;
+using PetMarketRfullApi.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyConString")));
 
 builder.Services.AddScoped<ICategoryServices, CategoryService>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
